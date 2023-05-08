117455879d44538efe770fc2b99977aeec665038
nggon-snlsoft <120443757+nggon-snlsoft@users.noreply.github.com>
2023-05-08 19:22:37 +0900

    update db dump

--- game/_server/game/starter.bat
+++
@@ -1,5 +0,0 @@
-cd ..
-git pull
-cd server
-forever stopall
-NODE_ENV='production' forever start -a -l "C:\_works\projectTH\server\lib\logs\log.log" -e "C:\_works\projectTH\server\lib\logs\err.log" -m 5 ./lib/index.js
